a4634e0dc35ea6e8088ee71519399a951f72c5a7
saijalg <151684512+saijalg@users.noreply.github.com>
2023-12-10 20:00:08 +0530

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -34,8 +34,8 @@ const createFooter = () => {
         </div>
     </div>
     <p class="footer-title">about company</p>
-    <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores, natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
-    <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+    <p class="info">Zengeez, the embodiment of style, confidence, and innovation, continues to make waves in the fashion industry. As an apparel brand that goes beyond the conventional, Zengeez invites individuals to embrace their unique style journeys. With a commitment to sustainability, innovation, and community, Zengeez is not just a brand; it's a lifestyle that encourages everyone to express their authenticity with confidence.</p>
+    <p class="info">support emails - help@zengeez.com, customersupport@zengeez.com</p>
     <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
     <div class="footer-social-container">
         <div>
